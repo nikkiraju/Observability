5f5047851e7ca785ef881905c91beb30c72bf7dc
Nikitha Chintalapati <nikkiraju1@gmail.com>
2024-04-23 14:22:57 -0400

    added files

---
+++ tracing.js
@@ -0,0 +1,38 @@
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { trace } = require("@opentelemetry/api");
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
+const jaegerExporter = new JaegerExporter({
+    serviceName: 'observability',
+    endpoint: 'http://localhost:14268/api/traces',
+});
+
+module.exports = (serviceName) => {
+    const provider = new NodeTracerProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+        }),
+    });
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+
+    registerInstrumentations({
+        instrumentations: [
+            new HttpInstrumentation(),
+            new ExpressInstrumentation(),
+            new MongoDBInstrumentation(),
+        ],
+        tracerProvider: provider,
+    });
+
+    provider.register();
+
+    return trace.getTracer(serviceName);
+};
